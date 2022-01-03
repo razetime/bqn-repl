d83325369e4f06bad4ab5e283d08e0306869ccac
razetime <raghuallthetime@hotmail.com>
2022-01-03 12:36:16 +0530

    add lb, cheatsheet

--- main.js
+++ main.js
@@ -24,10 +24,35 @@ code.addEventListener("keydown", (e) => {
         console.log(str);
         let out = "";
         if(str == ")h") {out = `REPL Help:
+)lb - show BQN keyboard
+)cmds - show all commands
 )clear - clears all variables
 )cls - clears all text on screen.`; }
         else if(str == ")clear") { prelude = ""; }
         else if(str == ")cls") { code.value = ""; }
+        else if(str == ")cmds") { out = `+   +  conjugate / add           ⥊  \\z  deshape / reshape
+-   -  negate / subtract         ∾  \\,  join / join to
+×  \\=  sign / multiply           ≍  \\.  solo / couple
+÷  \\-  reciprocal / divide       ↑  \\r  prefixes / take
+⋆  \\+  exponential / power       ↓  \\c  sufixes / drop
+√  \\_  square root / nth root    ↕  \\d  range / windows
+⌊  \\b  floor / minimum           «  \\H  nudge / shift before
+⌈  \\B  ceiling / maximum         »  \\L  nudge after / shift after
+∧  \\t  sort up / and             ⌽  \\q  reverse / rotate
+∨  \\v  sort down / or            ⍉  \\a  transpose / reorder axes
+¬  \\~  not / span                /   /  indices / replicate
+|  |   absolute value / modulus  ⊏  \\i  first cell / select
+≤  \\<  less than or equal to     ⊑  \\I  first / pick
+<   <  enclose / less than       ⊐  \\o  classify / index of
+>   >  merge / greater than      ⊒  \\O  occurence count / progressive index of
+≥  \\>  greater than or equal to  ⍋  \\T  grade up / bins up
+=   =  rank / equals             ⍒  \\V  grade down / bins down
+≠  \\/  length / not equals       ∊  \\e  mark first / member of
+≡  \\m  depth / match             ⍷  \\E  deduplicate / find
+≢  \\M  shape / not match         ⊔  \\u  group indices / group
+⊣  \\{  identity / left           !   !  assert / assert with message
+⊢  \\}  identity / right`; }
+        else if(str == ")lb") {let d=document;let e=d.createElement("script");e.src="https://abrudz.github.io/lb/bqn.js";d.body.appendChild(e)}
         else { try {
             out = fmt(bqn(prelude + str));
             if(str.match(/\b_?[a-z¯π∞0-9_]+_?\s*[←↩⇐]/i)) {
